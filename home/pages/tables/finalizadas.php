24044861b6e0e01fac6c8e53b168ceb053e065a8
Esler Escobar <escobarcarpio@gmail.com>
2024-10-23 14:15:07 -0600

    cambios en el titulo

--- home/pages/tables/finalizadas.php
+++ home/pages/tables/finalizadas.php
@@ -24,7 +24,7 @@ $pageend = 5;
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>iThink | Web </title>
+  <title>iThink | Finalizadas </title>
   <link rel="icon" href="/home/dist/img/logo_ithink.png" type="image/x-icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
