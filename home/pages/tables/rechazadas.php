01f620b69f98c722bc56076467178788c0a34dcf
Esler Escobar <escobarcarpio@gmail.com>
2024-10-23 14:13:13 -0600

    cambios 2

--- home/pages/tables/rechazadas.php
+++ home/pages/tables/rechazadas.php
@@ -26,7 +26,7 @@ include("../../logica/ac_permiso.php");
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>iThink | Web </title>
+  <title>iThink | Liberaciones Rechazadas </title>
   <link rel="icon" href="/home/dist/img/logo_ithink.png" type="image/x-icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
