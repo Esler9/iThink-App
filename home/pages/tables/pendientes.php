38dc035a0ee461062e89e57b4cbb2f916f3ce794
Esler Escobar <escobarcarpio@gmail.com>
2024-10-23 14:12:15 -0600

    cambios

--- home/pages/tables/pendientes.php
+++ home/pages/tables/pendientes.php
@@ -33,7 +33,7 @@ if (Tiene_permiso($permisos_user,'ver-pendientes-lb')) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>iThink | Web </title>
+  <title>iThink | Pendientes </title>
   <link rel="icon" href="/home/dist/img/logo_ithink.png" type="image/x-icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
