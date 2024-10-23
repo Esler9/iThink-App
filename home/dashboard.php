738aaa02d3d0d9e9f69659770fdda22e4de86b99
Esler Escobar <escobarcarpio@gmail.com>
2024-10-22 21:11:27 -0600

    2

--- home/dashboard.php
+++ home/dashboard.php
@@ -4,6 +4,11 @@ session_start();
 $User = $_SESSION["username"];
 $cod_user = $_SESSION['cod_user'];
 
+include ('../Setting.php');
+
+if($mantenimiento == true ){
+  header('location:../mantenimiento.php');
+  exit;}
 
 if(!isset($User)){
   header('location:login.php');
