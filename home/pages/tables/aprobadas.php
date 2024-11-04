3846adac7aa5c3e9b6c5fbf8dd778c44ce967269
Esler Escobar <escobarcarpio@gmail.com>
2024-11-03 21:37:20 -0600

    layaut flex

--- home/pages/tables/aprobadas.php
+++ home/pages/tables/aprobadas.php
@@ -48,7 +48,7 @@ if (Tiene_permiso($permisos_user,'ver-aprobadas-lb')) {
   <link rel="stylesheet" href="../../dist/css/app.css">
 
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
 
  
       <?php include("sidebar.php");?>
