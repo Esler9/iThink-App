f8663fe2e71a1b686ac42bc29c54da0b768526b1
Esler Escobar <escobarcarpio@gmail.com>
2025-02-08 16:29:46 -0600

    dfs

--- home/dashboard.php
+++ home/dashboard.php
@@ -103,7 +103,7 @@ $tiendas = $_SESSION['tiendas'];
                     echo $row['consultas'];
                     ?>
                   </h3>
-                  <p>Pendientes 2</p>
+                  <p>Pendientes</p>
                 </div>
                 <div class="icon">
                   <i class="fas fa-solid fa-exclamation"></i>
