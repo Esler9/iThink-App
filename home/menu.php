578ace9c74356b09ce006c2bd52fbe24e1db9eec
Esler Escobar <escobarcarpio@gmail.com>
2025-02-11 15:01:45 -0600

    fg

--- home/menu.php
+++ home/menu.php
@@ -334,17 +334,6 @@
             </nav>
         </div>
     </aside>
-
-    <!-- Content Wrapper -->
-    <div class="content-wrapper">
-        <!-- Contenido Principal -->
-        <section class="content">
-            <div class="container-fluid">
-                <h2>Bienvenido a iThink-App</h2>
-                <p>Selecciona una opción del menú para comenzar.</p>
-            </div>
-        </section>
-    </div>
 </div>
 
 <!-- JavaScript -->
