1657022d50c944a059455954cfc746d317da4368
Esler Escobar <escobarcarpio@gmail.com>
2025-02-11 17:32:29 -0600

    sdad

--- home/menu.php
+++ home/menu.php
@@ -399,7 +399,7 @@
   }
   .main-header .nav-link,
   .main-sidebar .nav-link {
-      color: #000000;
+      color:  #515a5a !important;
   }
   .main-header .nav-link:hover,
   .main-sidebar .nav-link:hover {
