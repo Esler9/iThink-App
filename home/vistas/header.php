a2813c4fbd203344edf987bb6306f989f456915e
Esler Escobar <escobarcarpio@gmail.com>
2025-02-15 16:56:28 -0600

    Actualizar rutas de inclusión en header.php para usar rutas absolutas y mejorar la gestión de sesiones

--- home/vistas/header.php
+++ home/vistas/header.php
@@ -3,23 +3,23 @@ session_start();
 $User = $_SESSION["username"];
 $cod_user = $_SESSION['cod_user'];
 
-include('../../Setting.php');
+include($_SERVER['DOCUMENT_ROOT'].'/home/Setting.php');
 
 if ($mantenimiento == true) {
-  header('location:../../mantenimiento.php');
+  header('Location: /home/mantenimiento.php');
   exit;
 }
 
 if (!isset($User)) {
-  header('location:login.php');
+  header('Location: /home/login.php');
   exit();
 }
 
-include("../../conexion.php");
-include("../logica/ac_permiso.php");
+include($_SERVER['DOCUMENT_ROOT'].'/home/conexion.php');
+include($_SERVER['DOCUMENT_ROOT'].'/home/vistas/logica/ac_permiso.php');
 
 // Obtención de tiendas a las que tiene acceso el usuario
-$sql = "SELECT cod_tienda FROM `asignacion_tienda` where cod_user = $cod_user";
+$sql = "SELECT cod_tienda FROM `asignacion_tienda` WHERE cod_user = $cod_user";
 $consulta = mysqli_query($conn, $sql);
 while ($fila = mysqli_fetch_array($consulta)) {
   $marcas[] = $fila['cod_tienda'];
