04acf05d38e29a37ca65048c6f6f68d4bf013dfb
Esler Escobar <escobarcarpio@gmail.com>
2025-02-15 16:33:17 -0600

    Refactor rutas en el header para usar rutas absolutas y mejorar la gestión de sesiones

--- home/vistas/header.php
+++ home/vistas/header.php
@@ -1,32 +1,40 @@
 <?php
 session_start();
-$User = $_SESSION["username"];
-$cod_user = $_SESSION['cod_user'];
 
-include('../Setting.php');
+$User = $_SESSION["username"];
+$cod_user = $_SESSION["cod_user"];
+
+// Definir la ruta base global
+$basePath = $_SERVER["DOCUMENT_ROOT"] . "/home";
+
+// Incluir archivos usando rutas absolutas
+include($basePath . "/Setting.php");
 
 if ($mantenimiento == true) {
-  header('location:../mantenimiento.php');
-  exit;
+    header("Location: " . $basePath . "/mantenimiento.php");
+    exit;
 }
 
 if (!isset($User)) {
-  header('location:login.php');
-  exit();
+    header("Location: " . $basePath . "/login.php");
+    exit();
 }
 
-include("../conexion.php");
-include("logica/ac_permiso.php");
+include($basePath . "/conexion.php");
+include($basePath . "/vistas/logica/ac_permiso.php");
 
 // Obtención de tiendas a las que tiene acceso el usuario
-$sql = "SELECT cod_tienda FROM `asignacion_tienda` where cod_user = $cod_user";
+$sql = "SELECT cod_tienda FROM asignacion_tienda WHERE cod_user = $cod_user";
 $consulta = mysqli_query($conn, $sql);
+$marcas = array();
 while ($fila = mysqli_fetch_array($consulta)) {
-  $marcas[] = $fila['cod_tienda'];
+    $marcas[] = $fila['cod_tienda'];
 }
 if (!empty($marcas)) {
-  $tiendas = implode(', ', $marcas);
-  $_SESSION['tiendas'] = $tiendas;
+    $tiendas = implode(', ', $marcas);
+    $_SESSION['tiendas'] = $tiendas;
+} else {
+    $_SESSION['tiendas'] = "";
 }
 $tiendas = $_SESSION['tiendas'];
 ?>
