08849b19b493644a6b951b9dc10b720c775f2596
Esler Escobar <escobarcarpio@gmail.com>
2025-02-15 16:58:43 -0600

    Agregar gestión de sesiones y verificación de mantenimiento en dashboard.php

--- home/dashboard.php
+++ home/dashboard.php
@@ -1,6 +1,35 @@
 <!DOCTYPE html>
 <?php
-include("vistas/header.php");
+session_start();
+$User = $_SESSION["username"];
+$cod_user = $_SESSION['cod_user'];
+
+include('../Setting.php');
+
+if ($mantenimiento == true) {
+  header('location:../mantenimiento.php');
+  exit;
+}
+
+if (!isset($User)) {
+  header('location:login.php');
+  exit();
+}
+
+include("../conexion.php");
+include("logica/ac_permiso.php");
+
+// Obtención de tiendas a las que tiene acceso el usuario
+$sql = "SELECT cod_tienda FROM `asignacion_tienda` where cod_user = $cod_user";
+$consulta = mysqli_query($conn, $sql);
+while ($fila = mysqli_fetch_array($consulta)) {
+  $marcas[] = $fila['cod_tienda'];
+}
+if (!empty($marcas)) {
+  $tiendas = implode(', ', $marcas);
+  $_SESSION['tiendas'] = $tiendas;
+}
+$tiendas = $_SESSION['tiendas'];
 ?>
 
 <html lang="es">
