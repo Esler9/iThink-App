a96db531ede0f84e1f3fb08772941ed7a80c28a9
Esler Escobar <escobarcarpio@gmail.com>
2025-03-07 22:08:17 -0600

    Eliminar la inclusión de la barra lateral en el formulario de permisos

--- home/pages/admin_permisos.php
+++ home/pages/admin_permisos.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Formulario de Permisos</title>
 </head>
 <body>
-<?php include("sidebar.php"); ?>
+
 
     <div class="container mt-5">
         <?php if (isset($_GET['msg'])): ?>
