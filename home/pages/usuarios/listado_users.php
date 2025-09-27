3af412a165ee4d078269b2110c602c65cdbddc1e
Esler Escobar <escobarcarpio@gmail.com>
2025-09-27 15:51:18 -0600

    Agregar verificación de sesión en listado_users.php para redirigir a login si no están configuradas

--- home/pages/usuarios/listado_users.php
+++ home/pages/usuarios/listado_users.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+// Verificar que las variables de sesión estén configuradas
+if (!isset($_SESSION["username"]) || !isset($_SESSION['cod_user'])) {
+  header('Location: ../../login.php');
+  exit();
+}
 
-
-include("sidebar.php");
+include("../../../conexion.php");
+include("../../logica/ac_permiso.php");
