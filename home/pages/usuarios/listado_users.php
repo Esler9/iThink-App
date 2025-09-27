795def18eb90146fa72d381854690266e9c80ef6
Esler Escobar <escobarcarpio@gmail.com>
2025-09-27 15:53:01 -0600

    Agregar declaración DOCTYPE y mejorar la estructura del encabezado en listado_users.php

--- home/pages/usuarios/listado_users.php
+++ home/pages/usuarios/listado_users.php
@@ -1,3 +1,4 @@
+<!DOCTYPE php>
 <?php
 session_start();
 // Verificar que las variables de sesión estén configuradas
@@ -7,4 +8,24 @@ if (!isset($_SESSION["username"]) || !isset($_SESSION['cod_user'])) {
 }
 
 include("../../../conexion.php");
-include("../../logica/ac_permiso.php");
+include("../../logica/ac_permiso.php"); ?>
+
+<php lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>iThink | Usuarios </title>
+  <link rel="icon" href="/home/dist/img/logo_ithink.png" type="image/x-icon">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../../dist/css/app.css">
+
+</head>
