35a6192611fc95a0385da49bdbbc4918800ce974
Esler Escobar <escobarcarpio@gmail.com>
2025-09-27 16:07:44 -0600

    Eliminar línea en blanco innecesaria en listado_users.php

--- home/pages/usuarios/listado_users.php
+++ home/pages/usuarios/listado_users.php
@@ -50,6 +50,24 @@ if (Tiene_permiso($permisos_user,'ver-usuarios')) {
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
-
  
       <?php include("sidebar.php");?>
+      
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Aprobadas</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="../../dashboard.php">Home</a></li>
+              <li class="breadcrumb-item active">Aprobadas</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
