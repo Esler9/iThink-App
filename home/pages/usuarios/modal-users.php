bba67bb10d0987fefce406e3510a06dbd495faf2
Esler Escobar <escobarcarpio@gmail.com>
2025-09-28 14:26:51 -0600

    Refactor modal-users.php: se corrigen las rutas de acción en los formularios para eliminar la extensión .php, mejorando la consistencia en el código.

--- home/pages/usuarios/modal-users.php
+++ home/pages/usuarios/modal-users.php
@@ -7,7 +7,7 @@ if (!isset($states) || !is_array($states)) $states = [];
 <!-- Crear Usuario -->
 <div class="modal fade" id="createUserModal" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
-    <form id="formCreateUser" method="post" action="../../logica/Usuarios/accion_usuarios.php?accion=0" class="modal-content">
+    <form id="formCreateUser" method="post" action="../../logica/Usuarios/accion_usuarios?accion=0" class="modal-content">
       <input type="hidden" name="__debug_post_check" value="1" />
       <div class="modal-header">
         <h5 class="modal-title">Crear usuario</h5>
@@ -91,7 +91,7 @@ if (!isset($states) || !is_array($states)) $states = [];
 <!-- Editar Usuario -->
 <div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
-    <form id="formEditUser" method="post" action="../../logica/Usuarios/accion_usuarios.php?accion=1" class="modal-content">
+    <form id="formEditUser" method="post" action="../../logica/Usuarios/accion_usuarios?accion=1" class="modal-content">
       <input type="hidden" name="__debug_post_check" value="1" />
       <input type="hidden" name="codigo" id="e_codigo" value="" />
       <div class="modal-header">
@@ -194,7 +194,7 @@ if (!isset($states) || !is_array($states)) $states = [];
 <!-- Eliminar Usuario -->
 <div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <form id="formDeleteUser" method="post" action="../../logica/Usuarios/accion_usuarios.php?accion=2" class="modal-content">
+    <form id="formDeleteUser" method="post" action="../../logica/Usuarios/accion_usuarios?accion=2" class="modal-content">
       <input type="hidden" name="__debug_post_check" value="1" />
       <input type="hidden" name="codigo" id="d_codigo" value="" />
       <div class="modal-header"><h5 class="modal-title">Eliminar usuario</h5>
